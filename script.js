0e92c73a3cda8d1a6483ea7d43ec3a28e68024ec
T-ramya-sree <85395315+T-ramya-sree@users.noreply.github.com>
2026-06-24 12:05:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,6 +4,21 @@ document.body.appendChild(getSumBtn);
 
 const getSum = () => {
 //Add your code here
+
+	const prices=document.querySelectorAll(".prices");
+	let total=0;
+	prices.forEach(price => {
+		total +=Number(price.textContent);
+	});
+	const tr=document.createElement("tr");
+	const td=document.createElement("td");
+
+	td.colSpan=2;
+	td.textContent=total;
+
+	tr.appendChild(td);
+
+	document.querySelector("table").appendChild(tr);
   
 };
 
